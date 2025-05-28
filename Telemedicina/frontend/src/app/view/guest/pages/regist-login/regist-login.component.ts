c3716f8d50090d26b27c0ad7e1b69e7434b69f1c
Fejér Gergő <h157480@stud.u-szeged.hu>
2025-05-28 11:56:31 +0200

    Ionic bugifek regist-login oldalon és a formokban.

--- Telemedicina/frontend/src/app/view/guest/pages/regist-login/regist-login.component.ts
+++ Telemedicina/frontend/src/app/view/guest/pages/regist-login/regist-login.component.ts
@@ -2,7 +2,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { RouterModule, ActivatedRoute } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 import { LoginFormComponent } from './forms/login-form/login-form.component';
 import { PatientRegistComponent } from './forms/patient-regist/patient-regist.component';
@@ -24,34 +24,7 @@ import { DoctorRegistComponent } from './forms/doctor-regist/doctor-regist.compo
   ]
 })
 export class RegistLoginComponent {
-  isMobile: boolean = false;
-  private resizeListener!: () => void;
   activeTab: 'login' | 'patient' | 'doctor' = 'login';
 
-  constructor(private route: ActivatedRoute) {}
-
-  ngOnInit() {
-    this.updateScreenSize();
-    this.resizeListener = () => this.updateScreenSize();
-    window.addEventListener('resize', this.resizeListener);
-
-    this.route.queryParams.subscribe(params => {
-      const tab = params['tab'] as 'login' | 'patient' | 'doctor';
-      if (tab === 'login' || tab === 'patient' || tab === 'doctor') {
-        this.activeTab = tab;
-      }
-    });
-  }
-
-  ngOnDestroy(): void {
-    window.removeEventListener('resize', this.resizeListener);
-  }
-
-  updateScreenSize() {
-    this.isMobile = window.innerWidth < 768;
-
-    const viewportHeight = window.visualViewport?.height || window.innerHeight;
-    document.documentElement.style.setProperty('--content-height', `${viewportHeight}px`);
-  }
-
+  constructor() {}
 }
