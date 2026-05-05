21254d5d681512eb9108a46c3564ab7bf15482bd
Fejér Gergő <fejer.gergo44@gmail.com>
2026-05-05 17:15:23 +0200

    frontend kulcs modositas

--- Telemedicina/frontend/enviroment.ts
+++ Telemedicina/frontend/enviroment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: '/api',
-  supabaseUrl: 'https://ubesundbzjtyxxuwmbgg.supabase.co',
+  supabaseUrl: 'https://vozjenetdqbetssbrrvk.supabase.co',
   supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZvemplbmV0ZHFiZXRzc2JycnZrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc5Mjk2NDAsImV4cCI6MjA5MzUwNTY0MH0.dTJfOuQDN52lKIhn49Kt-GP2pV1u830DCFGfrM9_ZgI',
   cryptoKey: 'itt_a_supabase_jwt_secret'
 };
