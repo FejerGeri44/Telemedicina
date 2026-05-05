9c804f9e7d35b3c53c540f512188969ca1b68e96
Fejér Gergő <fejer.gergo44@gmail.com>
2026-05-05 09:31:18 +0200

    Revert "Adatbázis módosítas"
    This reverts commit 653eadd6ea71bde9c52884a8df6d5837c03bc479.

--- Telemedicina/frontend/enviroment.ts
+++ Telemedicina/frontend/enviroment.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   apiUrl: '/api',
   supabaseUrl: 'https://ubesundbzjtyxxuwmbgg.supabase.co',
-  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZvemplbmV0ZHFiZXRzc2JycnZrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc5Mjk2NDAsImV4cCI6MjA5MzUwNTY0MH0.dTJfOuQDN52lKIhn49Kt-GP2pV1u830DCFGfrM9_ZgI',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InViZXN1bmRiemp0eXh4dXdtYmdnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE1MDUwODUsImV4cCI6MjA3NzA4MTA4NX0.HKqqPqIlNrJGTNqsgLg2YsHwghj3e-HWpCRegrTjBaY',
   cryptoKey: 'itt_a_supabase_jwt_secret'
 };
